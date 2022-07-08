6a2bb37266e8883e38b86b2f13f9387f42b5d932
Nick <mr.upgrade3@protonmail.com>
2022-07-08 20:11:10 +0300

    Add solution for task (String Repeat) from CodeWars.com

---
+++ 8kyo/script_StringRepeat.js
@@ -0,0 +1,12 @@
+/*
+    Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+
+    Examples (input -> output)
+
+    "I"     -> "IIIIII"
+    "Hello" -> "HelloHelloHelloHelloHello"
+*/
+
+function repeatStr (n, s) {
+    return s.repeat(n);
+}
