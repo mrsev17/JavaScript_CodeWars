b7cdae33e54f81fadc0c6b761dbef11d301a3ad4
Nick <mr.upgrade3@protonmail.com>
2022-07-24 01:20:59 +0300

    Add solution for task (Fundamentals:Return) from CodeWars.com

--- 8kyo/script_IsTheStringUppercase.js
+++ 8kyo/script_IsTheStringUppercase.js
@@ -20,17 +20,28 @@
     considered to be in ALL CAPS.
 */
 
-function upperOrLower(input) {
-    const newArr = input.split("");
-    if (newArr === undefined) {
-        return false;
-
-        for (let i = 0; i < newArr.length; i++) {
-            if (newArr[i] === newArr[i].toUpperCase()) {
-                return true;
-            } else {
-                return false;
-            }
+/*function upperOrLower(input) {
+    if (input === undefined) return false;
+    const testArr = input.split("");
+    let strAfterCheck = "";
+    for (let i = 0; i < testArr.length; i += 1) {
+        if (testArr[i] === testArr[i].toUpperCase()) {
+            strAfterCheck += testArr[i];
         }
     }
+
+    console.log(input);
+    console.log(strAfterCheck);
+
+    if (strAfterCheck.length === input.length) return true;
+    if (strAfterCheck.length !== input.length) return false;
 }
+
+ */
+
+const checkChar = str => {
+    const isUpperCase = str => str === str.toUpperCase();
+    return isUpperCase();
+};
+
+// Already make two solutions for this kata but not of them is passing tests...
