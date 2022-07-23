cb4c6bf94519f58cb8ae01a8d505e6c4b91232c7
Nick <mr.upgrade3@protonmail.com>
2022-07-23 23:26:12 +0300

    Add solution for task (Find Maximum and Minimum Values of a lIst) from CodeWars.com

---
+++ 8kyo/script_FindMaximumAndMinimum.js
@@ -0,0 +1,17 @@
+/*
+     Your task is to make two functions
+     ( max and min, or maximum and minimum,
+     etc., depending on the language ) that
+     receive a list of integers as input, and
+     return the largest and lowest number in
+     that list, respectively.
+*/
+
+
+const minVal = enterArr => {
+    return Math.min.apply(null, enterArr);
+};
+
+const maxVal = data => {
+    return Math.max.apply(null, data);
+};
